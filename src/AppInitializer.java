a561ee7ad46fe80d6e98a6a2bff2fe9e32849e0d
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2020-07-17 16:23:35 +0530

    Load the Dashboard

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,6 +1,10 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AppInitializer extends Application {
 
@@ -9,7 +13,13 @@ public class AppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/Dashboard.fxml"));
+        Scene mainScene = new Scene(root);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("Team Work");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        primaryStage.centerOnScreen();
     }
 }
